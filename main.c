d377bb35a18d4a9f31cc97607c2aa955aaa341a4
Ylli Fazlija <fazlijaylli@gmail.com>
2022-04-26 11:14:09 +0200

    Canvas Programme + Petit modification du CMAKE

--- main.c
+++ main.c
@@ -1,3 +1,23 @@
-int main() {
-   return 0;
+/*
+ -----------------------------------------------------------------------------------
+ Nom du fichier : main.c
+ Auteur(s)      : Ylli Fazlija
+                  Kylian Manzini
+ Date creation  : 26.04.22
+
+ Description    : Laboratoire 01 de PRG2 : Simulation d'une planche de type "Galton Board".
+                  Affichage du nombre de billes à chaques étages de la planche, ainsi qu'un histogramme
+                  de la ditribution des billes.
+
+ Remarque(s)    : -
+
+ Compilateur    : Mingw-w64 gcc 11.2.0
+ -----------------------------------------------------------------------------------
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+   return EXIT_SUCCESS;
 }
